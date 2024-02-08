e2c5c583800a778d25d1eee2c8bf278af70aa065
Swarn Singh <swarn2099@gmail.com>
2024-02-08 17:28:36 -0600

    Initial Commit

---
+++ Amadeus/AmadeusApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmadeusApp.swift
+//  Amadeus
+//
+//  Created by Swarn Singh on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct AmadeusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
